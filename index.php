8210ce6f964ef8668e8c0ce3607ccc91f487711b
root <root@Wojtek.com>
2017-09-10 20:48:28 +0200

    Start working for navbar and login form

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+//przenoszenie do strony glownej usera
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <link href="style/tags.css" rel="stylesheet">
+        <link href="style/classes.css" rel="stylesheet">
+        <script src="jquery-3.2.1.min.js"></script>
+        <title>Enter - simple chat</title>
+    </head>
+    <body>
+        <nav>
+            <form method="post">
+                <div class="logo">
+                    <input type="submit" value="" name="">
+                </div>
+            </form>
+        </nav>
+    </body>
+</html>
