56d4c3ea65e716b632b3e4bfcf28b31472a1fc36
root <root@Wojtek.com>
2017-09-12 22:38:28 +0200

    Repaired navbar, created sections, now beign login system.

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
         <link href="style/tags.css" rel="stylesheet">
         <link href="style/classes.css" rel="stylesheet">
         <link href="style/buttons/UI-Button/button.css" rel="stylesheet">
+        <link href="style/ids.css" rel="stylesheet">
         <script src="jquery-3.2.1.min.js"></script>
         <title>Enter - simple chat</title>
     </head>
@@ -18,5 +19,21 @@
                 <button id='login' class='ui basic green button'>Log In</button>
             </div>
         </nav>
+        <main>
+            <section id='about'>
+             <article>
+                <header>
+                    <h1>About site</h1>
+                </header>
+             </article>
+            </section>
+            <section id='register'>
+             <article>
+                <header>
+                    <h1></h1>
+                </header>
+             </article>
+            </section>
+        </main>
     </body>
 </html>
