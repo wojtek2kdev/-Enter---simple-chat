216325bdf00aa484a62ba35c7d4f922a87b6f915
root <root@Wojtek.com>
2017-09-29 14:43:22 +0200

    Little correction in nav

--- welcome.php
+++ welcome.php
@@ -7,7 +7,11 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title></title>
+    <link href="style/tags.css" rel="stylesheet">
+    <link href="style/classes.css" rel="stylesheet">
+    <link href="style/buttons/UI-Button/button.css" rel="stylesheet">
+    <link href="style/ids.css" rel="stylesheet">
+    <title>Welcome to 'Enter' chat!</title>
 </head>
 <body>
    <?php echo(Element::getElement('nav')); ?>
