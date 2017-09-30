0ae92db76036513e350d4136d2dc2916a8199561
root <root@Wojtek.com>
2017-09-30 21:32:47 +0200

    Simple login form skeleton

--- login.php
+++ login.php
@@ -11,10 +11,20 @@
     <link href="style/classes.css" rel="stylesheet">
     <link href="style/buttons/UI-Button/button.css" rel="stylesheet">
     <link href="style/ids.css" rel="stylesheet">
+    <link href="./style/login-page/classes.css" rel="stylesheet">
     <title>Enter - Log in</title>
 </head>
 <body>
     <?php echo(Element::getElement('nav')); ?>
     <script type="text/javascript">document.getElementsByClassName('login')[0].remove();</script>
+    <div class="login-form">
+        <h1>Enter - simple chat.</h1>
+        <h3>Log in to your account.</h3>
+        <form method="post">
+            <input type="text" placeholder="Type your login.." name="login">
+            <input type="password" placeholder="Type your password.." name="password">
+            <button>Log in</button>
+        </form>
+    </div>
 </body>
 </html>
