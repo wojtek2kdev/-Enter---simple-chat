fe92302661bc463fe000ef8742d2f0817f395fec
root <root@Wojtek.com>
2017-10-21 16:46:14 +0200

    Not showing user if is friend

---
+++ php/search_user.php
@@ -0,0 +1,25 @@
+<?php
+
+  require_once(__DIR__.'/user/account.php');
+
+    session_start();
+
+    if(isset($_POST['user']) && isset($_SESSION['nick'])){
+        if(strlen($_POST['user'])){
+          $arr = iterator_to_array(Account::searchUser($_POST['user']));
+          $i = array_search([$_SESSION['nick']], $arr);
+          if($i !== false) array_splice($arr, $i, 1);
+          foreach(iterator_to_array(Account::getFriends()) as $friend){
+            $f = array_search($friend, $arr);
+            if($f !== false) array_splice($arr, $f, 1);
+          }
+          if(count($arr) > 50){
+            $arr = array_slice($arr, 0, 50);
+          }
+        echo(json_encode($arr));
+        }
+    }else{
+      header('Location: /');
+    }
+
+?>
